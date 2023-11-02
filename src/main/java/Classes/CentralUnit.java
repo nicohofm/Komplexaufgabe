1f5cd2eaf2c35f1f199a7584530031e781dd05b2
Nico Hofmann <nico.hofmann1998@gmx.de>
2023-11-02 16:11:10 +0100

    CentralUnit befülllt

--- src/main/java/Classes/CentralUnit.java
+++ src/main/java/Classes/CentralUnit.java
@@ -1,4 +1,30 @@
 package Classes;
 
+import Interfaces.IEncryption;
+import com.sun.source.tree.Tree;
+
+import java.awt.desktop.OpenFilesEvent;
+import java.util.TreeMap;
+
 public class CentralUnit {
+    private TreeMap<Integer, Officer> registeredOfficers;
+    private Record[] fineRecord;
+    private LogEngine logEngine;
+    private IEncryption encryptionSHA256;
+
+    public void FillRegisteredOfficers(){
+
+    }
+    public boolean CheckOfficer(int id, int pin){
+        return false;
+    }
+    public void AddRecord(Record record){
+
+    }
+    public void CreateReport(){
+
+    }
+    public void CreateExport(){
+
+    }
 }
