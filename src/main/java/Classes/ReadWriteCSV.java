96e935b370f301bd8e76c48aaaf95aa62a8b710e
Nico Hofmann <nico.hofmann1998@gmx.de>
2023-11-02 20:42:31 +0100

    Changes by Phil Steinruck

--- src/main/java/Classes/ReadWriteCSV.java
+++ src/main/java/Classes/ReadWriteCSV.java
@@ -1,10 +1,32 @@
 package Classes;
 import Interfaces.IReadWriteFile;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadWriteCSV implements IReadWriteFile{
+    private List<String[]> csvData = new ArrayList<>();
     @Override
     public void readFile(String path) {
+        try{
+            String line;
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+            while((line = bufferedReader.readLine()) != null)
+            {
+                csvData.add(line.split(";"));
+            }
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public List<String[]> getCsvData(String path)
+    {
+        readFile(path);
+        return csvData;
     }
 
     @Override
