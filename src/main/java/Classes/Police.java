9839353c7026224c3ca1fab417684b3f2a4215a3
Phil Steinruck <steinrph@gsmgh.net>
2023-11-02 15:38:15 +0100

    Added Methods and Attributes in class Police

--- src/main/java/Classes/Police.java
+++ src/main/java/Classes/Police.java
@@ -1,4 +1,13 @@
 package Classes;
 
 public class Police {
+    private String[] wantedOwner;
+    private Owner[] arrestedOwner;
+    private Car[] confiscatedCars;
+    //private ReadWriteCSV csvReader;
+    //private EncryptionAES encryptionAES;
+
+    public void fillWantedOwner(){}
+    public boolean isOwnerWanted(String face){return false;}
+
 }
